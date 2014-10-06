1b2efd82dcf9b43797b831787aa6b22bd454f83b
Tim Caplis <tim@haul.io>
2014-10-06 14:54:50 -0700

    tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,31 @@
-
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON( 'package.json' ),
-		emberhandlebars: {
-			compile: {
-				options: {
-					templateName: function(sourcefile){ 
-						var newSource = sourcefile.replace('static/templates/', '');
-						return newSource.replace('.hbs', ''); 
-					}
-				},
-				files: ['static/templates/*.hbs','static/templates/**/*.hbs'],
-				dest: 'static/js/templates.js'
-			}
-		},
-		watch: {
-			options: {
-				livereload: 35729,
-			},
-			emberhandlebars: {
-				files: ['static/templates/**/*.hbs', 'static/templats/*.hbs', 'static/css/*.css'],
-				tasks: ['emberhandlebars']
-			}
-		}
-	});
+    grunt.initConfig({
+        pkg: grunt.file.readJSON( 'package.json' ),
+        emberhandlebars: {
+            compile: {
+                options: {
+                    templateName: function(sourcefile){
+                        var newSource = sourcefile.replace('static/templates/', '');
+                        return newSource.replace('.hbs', '');
+                    }
+                },
+                files: ['static/templates/*.hbs','static/templates/**/*.hbs'],
+                dest: 'static/js/templates.js'
+            }
+        },
+        watch: {
+            options: {
+                livereload: 35729,
+            },
+            emberhandlebars: {
+                files: ['static/templates/**/*.hbs', 'static/templats/*.hbs', 'static/css/*.css'],
+                tasks: ['emberhandlebars']
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-watch'); 
-	grunt.loadNpmTasks('grunt-ember-template-compiler');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ember-template-compiler');
 
-	grunt.registerTask('default', ['emberhandlebars', 'watch']);
+    grunt.registerTask('default', ['emberhandlebars', 'watch']);
 };
