c6ac2b8ad4caa8a381e0060ee00c5172e8082ca1
Tim Caplis <tim@haul.io>
2014-10-11 12:19:55 -0700

    public apis on even ports

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,7 @@ Haul.Store = DS.Store.extend({
 Haul.TooltipBoxController = Bootstrap.TooltipBoxController
 
 //GLOABL SETTINGS.  HOW, WHERE SHOULD WE HANDLE THIS STUFF?
-Haul.IMAGE_SERVER_HOST = "http://localhost:8081";
+Haul.IMAGE_SERVER_HOST = "http://localhost:8082";
 Haul.USER_SERVER_HOST = "http://localhost:8080";
 Haul.CLIENT_TOKEN = "5eed07b8d71cf26f6df6566cf705adaa";
 Haul.FACEBOOK_APP_ID = "443672575768207";
