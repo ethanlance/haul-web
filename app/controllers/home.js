9200fd2b122dffdf07104213de0f28143b7b021d
ethanlance <ethan@haul.io>
2015-01-29 21:08:01 -0800

    the home page will be a feed.  for now the home page feed will be the logged in users posts.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,37 @@
-
 import Ember from 'ember';
 
+export default Ember.ObjectController.extend({
 
-	var HomeController = Ember.ObjectController.extend({
-		currentUserBinding: 'Haul.currentUser',
-	});
-	export default HomeController;
- 
+	currentUserIdBinding: 'Haul.currentUser.id',
+	currentUserBinding: 'Haul.currentUser',
+	isProfileOwner: true,  
+	model:false,
+
+	start: function() {
+			
+		var _this = this; 
+		var currentUser = this.get('currentUser');
+
+		
+		return this.store.find('collection', currentUser.get('collection').get('slug')).then(function(result){ 
+			_this.set('model', result);
+		}, function(error) {
+			console.log("ERROR", error);	
+		});
+
+
+	}.observes('currentUserId'), 
+
+	getProducts: function() {
+		if(!this.get('model').id){
+			return;
+		}
+
+		console.log("MODEL ", this.get('model'))
+		var model = this.get('model');
+		this.store.find("collection-product-list", {'collection_id':model.get('id')} )
+		.then(function(records){
+			model.set('products', records);
+		});
+	}.observes('model'),
+}); 
