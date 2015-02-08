711ec58d923f2597bca29bfa26be7464d271ac74
ethanlance <ethan@haul.io>
2015-02-08 00:25:29 -0800

    Proper ember way to open close MODAL dialogs.

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,21 @@ export default Ember.Route.extend({
 	actions: {
     	authenticateSession: function() {
       		this.transitionTo('signup');
-    	}
-  	}
+    	},
+  	 	
+		openModal: function(modalName, model) {
+			this.controllerFor(modalName).set('model', model);
+			return this.render(modalName, {
+				into: 'application',
+				outlet: 'modal'
+			});
+		},
+	    
+	    closeModal: function() {
+			return this.disconnectOutlet({
+				outlet: 'modal',
+				parentView: 'application'
+			});
+		}
+	}
 });
