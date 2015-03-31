ee0dcfe4e37d0204b124d5cd3b0aa6dbaa1acba8
ethanlance <ethan@haul.io>
2015-03-30 20:55:00 -0700

    prod stuff

--- Brocfile.js
+++ Brocfile.js
@@ -4,16 +4,24 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-	// vendorFiles: {
-	// 	'handlebars.js': null
-	// },
+	
 	//minifyJS:{'enabled':true, options:{'compress':true}}
+
+	emberCliFontAwesome: { includeFontAwesomeAssets: false },
+	
+	storeConfigInMeta: false,
+
+	fingerprint: {
+		prepend: 'https://s3.amazonaws.com/static.haul.io/'
+	},
+
 	sourcemaps: ['js'],
 
 	inlineContent: {
 		'braintree' : {file: 'public/assets/scripts/braintree.js'},
 		'twitter' 	: {file: 'public/assets/scripts/twitter.js'},
-		'facebook' 	: {file: 'public/assets/scripts/facebook.js'}
+		'facebook' 	: {file: 'public/assets/scripts/facebook.js'},
+		'pinterest' : {file: 'public/assets/scripts/pinterest.js'}
 	}
 });
 
@@ -30,9 +38,13 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 app.import('bower_components/jquery/dist/jquery.js');
+
 app.import('bower_components/underscore/underscore.js'); 
+
 app.import('bower_components/bootstrap/dist/js/bootstrap.js'); 
+
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
   destDir: 'fonts'
 });
@@ -40,40 +52,44 @@ app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.w
   destDir: 'fonts'
 });
 
-
 app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
   destDir: 'assets'
 });
 
-
 app.import('bower_components/ember/ember-template-compiler.js');
 
-
 app.import('bower_components/typeahead.js/dist/typeahead.bundle.js'); 
-app.import('vendor/jquery.elastic.source.js');
-app.import('vendor/jquery.mentionsInput.js');
-app.import('vendor/jquery.mentionsInput.css');
 
+app.import('vendor/jquery.elastic.source.js');
+
+app.import('vendor/jquery.mentionsInput.js');
+
+app.import('vendor/jquery.mentionsInput.css');
 
 app.import('bower_components/jquery-ui/jquery-ui.js');
 
 app.import('bower_components/moment/moment.js');
+
 app.import('bower_components/dropzone/downloads/dropzone.js');
 
-
-
 app.import('bower_components/ladda-bootstrap/dist/spin.js');
+
 app.import('bower_components/ladda-bootstrap/dist/ladda.js');
+
 app.import('bower_components/ladda-bootstrap/dist/ladda-themeless.css'); 
 
-// app.import('vendor/ember-easyform.js');
 app.import('vendor/ember-validations.js');
+
 app.import('bower_components/quill/dist/quill.js');
+
 app.import('vendor/quill.css');
 
+app.import("bower_components/font-awesome/css/font-awesome.css");
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
+
 module.exports = app.toTree();
-
-
-
-
-
