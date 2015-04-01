82c6ce8e67a76cd4a693382eb50b4fc0c4ab1c9f
ethanlance <ethan@haul.io>
2015-04-01 00:35:54 -0700

    Twitter button error fix.

--- app/components/twitter_btn.js
+++ app/components/twitter_btn.js
@@ -2,24 +2,30 @@ import Ember from 'ember';
 var $ = Ember.$;
 
 export default Ember.Component.extend({
-  tagName: 'a',
-  classNames: 'twitter-share-button',
-  attributeBindings: [
-    'data-size', 
-    'data-url', 
-    'data-text', 
-    'data-hashtags',
-    'data-count'
-  ],
+  	tagName: 'a',
+  	classNames: 'twitter-share-button',
+  	attributeBindings: [
+		'data-size', 
+		'data-url', 
+		'data-text', 
+		'data-hashtags',
+		'data-count'
+  	],
 
 
 
-  didInsertElement: function() {
-    
-      window.twttr.widgets.load();
-      $('iframe.twitter-share-button').css('width', '78px');
-    
-      this.set('data-url', window.location.href);  
-  }
+	didInsertElement: function() {
+		this.set('data-url', window.location.href);
 
+		function loadTwitter() {
+			if( window.twttr ){
+		  		window.twttr.widgets.load();
+		  		$('iframe.twitter-share-button').css('width', '78px');    
+		  		clearInterval(tInterval);
+			}
+		}
+
+	  	var tInterval = setInterval(function(){ loadTwitter() }, 500);
+		  
+	}
 });
