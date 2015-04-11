ecb910468db8698babb861043bbf18ef27f7f106
ethanlance <ethan@haul.io>
2015-04-10 17:23:59 -0700

    Fix the header on not-found and not-authorized.

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import AnonMixin from '../mixins/anon';
 export default Ember.Route.extend(AnonMixin, {
 	renderTemplate: function() {
-		this.render('layouts/header_anon', {
+		this.render('layouts/header_base', {
 			into: 'application',
 			outlet: 'header'
 		});
