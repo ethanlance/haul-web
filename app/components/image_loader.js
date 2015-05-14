617164b0cfe9b30601311dd50cadd0ee7bef08db
ethanlance <ethan@haul.io>
2015-05-13 17:56:25 -0700

    If image has not loaded for X milliseconds then load post anyway.

--- app/components/image_loader.js
+++ app/components/image_loader.js
@@ -1,27 +1,38 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
 	
+	wait: 1000,
+
 	loaded: false,
 
+	showImage: function() {
+		this.set('loaded', true);
 
+		var el = $('#'+ this.get('unHideId'));
+
+		el.removeClass('hide');
+	},
  	
  	handleLoad: function() {
  		var _this = this;
 		
+
+		//When the image loads send out an alert.
 		this.$().children('img').one('load', function() {
 		
 			Ember.run(function(){
-						
-				_this.set('loaded', true);
-
-				var el = $('#'+ _this.get('unHideId'));
-
-				el.removeClass('hide');
-		
+				_this.showImage();
 			});
 		
 		}.bind(this));
 	
+
+		//Regardless of if the image loads, after X milliseconds 
+		//send out an alert.
+		Ember.run.later(function(){
+			_this.showImage();
+		}, this.get('wait'));
+
 	}.on('didInsertElement')
 
 });
