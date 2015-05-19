8a779052e328836c22655193a521216596cfa55c
ethanlance <ethan@haul.io>
2015-05-19 02:16:57 -0700

    Edit your name cta

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';  
 export default Ember.ObjectController.extend({
-	needs: ['profile/index', 'profile/post/index'],
-	currentPage:'seller',
-	collection:null,
-	showGridBtn:false,
-	showGridView:false,
+	
+	currentUserIdBinding: 'session.currentUser.id',
+	
+	isProfileOwnerChanged: function() {
+		this.set('isProfileOwner', false); 
+		if( this.get('session').isAuthenticated && !Ember.isEmpty(this.get('currentUserId')) ) {
+			if (this.get('model').get('id') === this.get('currentUserId')) {
+				this.set('isProfileOwner', true);
+			}
+		} 
+	}.observes('model', 'currentUserId'),
+
 });
