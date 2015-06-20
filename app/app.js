873fcebedfbcddf3e316e37ae8debc33163d7d66
Tim Caplis <tim@haul.io>
2015-06-20 12:56:56 -0700

    google analytics

--- app/app.js
+++ app/app.js
@@ -11,6 +11,16 @@ var App = Ember.Application.extend({
   Resolver: Resolver
 });
 
+// TOOD: maybe there's a better place for this?
+Ember.Router.reopen({
+  notifyGoogleAnalytics: function() {
+    return __gaTrack('send', 'pageview', {
+      'page': this.get('url'),
+      'title': this.get('url')
+    });
+  }.on('didTransition')
+});
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
